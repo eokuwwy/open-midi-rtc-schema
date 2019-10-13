094ea435b51f718f399b7161bb5fb33642586c85
Steve Connelly <eokuwwy@gmail.com>
2019-10-13 11:44:12 -0500

    Web-based MIDI spec tools for testing

--- tools/generate-quicktype-models.sh
+++ tools/generate-quicktype-models.sh
@@ -3,6 +3,8 @@ mkdir $QUICKTYPE_DIR
 rm -rf $QUICKTYPE_DIR/*
 mkdir $QUICKTYPE_DIR/java
 mkdir $QUICKTYPE_DIR/swift
+mkdir $QUICKTYPE_DIR/js
 
 quicktype -s schema ../schema/open-midi-rtc-schema.json -o $QUICKTYPE_DIR/java/OpenMIDIRealtimeSpecification.java
 quicktype -s schema ../schema/open-midi-rtc-schema.json -o $QUICKTYPE_DIR/swift/OpenMIDIRealtimeSpecification.swift
+quicktype -s schema ../schema/open-midi-rtc-schema.json -o $QUICKTYPE_DIR/js/OpenMIDIRealtimeSpecification.js
